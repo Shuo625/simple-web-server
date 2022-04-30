ca4d6e2396b285c9ddc0e65ec2eb6d392a502b28
Shuo626 <qushuo233@gmail.com>
2022-04-30 14:15:37 +0100

    implement a simple server in rust

---
+++ simple-web-server-rust/src/main.rs
@@ -0,0 +1,35 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::fs;
+
+mod threadpool;
+
+use self::threadpool::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let threadpool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        threadpool.execute(|| { handle_connection(stream) });        
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
